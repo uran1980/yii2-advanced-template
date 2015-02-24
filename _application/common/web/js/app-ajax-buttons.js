2e0c804da1dffdf7300e580b7790e5ebf02f4352
uran1980 <uran1980@gmail.com>
2015-02-25 01:22:42 +0200

    refactoring

--- _application/common/web/js/app-ajax-buttons.js
+++ _application/common/web/js/app-ajax-buttons.js
@@ -18,22 +18,24 @@ var appAjaxButtons = appAjaxButtons || {};
         appAjaxButtons.ajaxButtonSubmit = function ( element ) {
             var url         = element.attr('href'),
                 icon        = element.find('i'),
-                iconClass   = icon.attr('class')
+                iconClass   = icon.attr('class'),
+                timeout     = 1200,
+                delay       = 2500
             ;
 
             if ( element.data('locked') === true ) {
                 return false;
             }
-            if ( iconClass ) {
-                icon.attr('class', 'fa fa-spinner fa-pulse');
-            }
 
             if ( url ) {
-                element.data('locked', true);
                 $.ajax({
                     type: 'POST',
                     url: url,
                     beforeSend: function ( xhr, settings ) {
+                        element.data('locked', true);
+                        if ( iconClass ) {
+                            icon.attr('class', 'fa fa-spinner fa-pulse');
+                        }
                         if ( element.attr('before-send-igrowl-message') ) {
                             // show iGrowl popup message
                             // @see http://catc.github.io/iGrowl/
@@ -44,63 +46,73 @@ var appAjaxButtons = appAjaxButtons || {};
                                     y: element.attr('placement-y') || 'top'
                                 },
                                 type:       'notice',
-                                delay:      element.attr('delay') || 10000,
-                                animation:  false,
-                                animShow:   'fadeInDown',
-                                animHide:   'fadeOutUp',
+                                delay:      delay * 10,
+                                animation:  true,
+                                animShow:   'fadeIn',
+                                animHide:   'fadeOut',
                                 title:      ':: ' + (element.attr('before-send-igrowl-title').toUpperCase() || 'REQUEST SENT') + ' .:',
                                 message:    element.attr('before-send-igrowl-message') || 'Please wait...'
                             });
                         }
                     },
                     success: function ( data ) {
-                        $.iGrowl.prototype.dismissAll('all');
-                        $.iGrowl({
-                            placement:  {
-                                x: element.attr('placement-x') || 'center',
-                                y: element.attr('placement-y') || 'top'
-                            },
-                            type:       data.status || 'success',
-                            delay:      element.attr('delay') || 2500,
-                            animation:  false,
-                            animShow:   'fadeInDown',
-                            animHide:   'fadeOutUp',
-                            title:      ':: ' + (element.attr('success-igrowl-title').toUpperCase() || 'SERVER RESPONSE') + ' .:',
-                            message:    element.attr('success-igrowl-message') || 'Success'
-                        });
-
-                        // triger event
-                        $(document).trigger('ajaxButtonSubmit', {
-                            element: element,
-                            url: url,
-                            action: element.attr('action'),
-                            data: data
-                        });
+                        window.setTimeout(function () {
+                            $.iGrowl.prototype.dismissAll('all');
+                            window.setTimeout(function () {
+                                $.iGrowl({
+                                    placement:  {
+                                        x: element.attr('placement-x') || 'center',
+                                        y: element.attr('placement-y') || 'top'
+                                    },
+                                    type:       data.status || 'success',
+                                    delay:      delay,
+                                    animation:  true,
+                                    animShow:   'fadeIn',
+                                    animHide:   'fadeOut',
+                                    title:      ':: ' + (element.attr('success-igrowl-title').toUpperCase() || 'SERVER RESPONSE') + ' .:',
+                                    message:    element.attr('success-igrowl-message') || 'Success'
+                                });
+                                if ( iconClass ) {
+                                    icon.attr('class', iconClass);
+                                }
+                                window.setTimeout(function () {element.data('locked', false);}, delay * 2);
+                                // triger event
+                                $(document).trigger('ajaxButtonSubmit', {
+                                    element: element,
+                                    url: url,
+                                    action: element.attr('action'),
+                                    data: data
+                                });
+                            }, timeout);
+                        }, timeout);
                     },
                 }).then(function () {                                           // doneCallbacks (@see http://api.jquery.com/deferred.then/)
                     // dummy
                 }, function ( xhr, errorType, exception ) {                     // failCallacks
-                    $.iGrowl.prototype.dismissAll('all')
-                    $.iGrowl({
-                        placement:  {
-                            x: element.attr('placement-x') || 'center',
-                            y: element.attr('placement-y') || 'top'
-                        },
-                        type:       'error',
-                        delay:      2500,
-//                        delay:      60000,                                      // test
-                        animation:  false,
-                        animShow:   'fadeInDown',
-                        animHide:   'fadeOutUp',
-                        title:      ':: SERVER ERROR .:',
-                        message:    xhr.responseText || 'Error'
-                    });
-                }).always(function () {
-                    element.data('locked', false);
-                    icon.attr('class', iconClass);
+                    window.setTimeout(function () {
+                        $.iGrowl.prototype.dismissAll('all');
+                        window.setTimeout(function () {
+                            $.iGrowl({
+                                placement:  {
+                                    x: element.attr('placement-x') || 'center',
+                                    y: element.attr('placement-y') || 'top'
+                                },
+                                type:       'error',
+                                delay:      delay,
+//                                delay:      delay * 60,                         // test
+                                animation:  true,
+                                animShow:   'fadeIn',
+                                animHide:   'fadeOut',
+                                title:      ':: SERVER ERROR .:',
+                                message:    xhr.responseText || 'Error'
+                            });
+                            if ( iconClass ) {
+                                icon.attr('class', iconClass);
+                            }
+                            window.setTimeout(function () {element.data('locked', false);}, delay * 2);
+                        }, timeout);
+                    }, timeout);
                 });
-            } else if ( iconClass ) {
-                icon.attr('class', iconClass);
             }
         };
 
